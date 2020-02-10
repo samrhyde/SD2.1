d68820315b049eb0983bee9ef6e3ecd187666a1c
Hyde <sh765515@ohio.edu>
2020-02-10 09:52:23 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HydeMvcP1.Startup))]
+namespace HydeMvcP1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
